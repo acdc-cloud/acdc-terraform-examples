db8e779df58ccf0f918335e1e392467dc26d5ed7
Holger Ihrig <holger@arcticcircledc.com>
2018-09-25 14:21:22 +0200

    Fix formatting

--- terraform/Mo/object-storage/main.tf
+++ terraform/Mo/object-storage/main.tf
@@ -19,7 +19,6 @@ provider "openstack" {
 resource "openstack_objectstorage_container_v1" "demo_private_container" {
   region = "RegionOne"
   name   = "demo"
-
 }
 
 # Create a public container that is world readable
@@ -33,3 +32,4 @@ resource "openstack_objectstorage_container_v1" "demo_public_container" {
 # Although it is possible to manage objects in containers with terraform, it is very tedious
 # and usually better accomplished by using some other tools, like the swift and s3cmd tools,
 # as files in containers are not really considerd infrastructure
+
