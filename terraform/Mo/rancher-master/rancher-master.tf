ef87b693414632d1e63268eaff5f68582cb2cbcf
Hallvard Westman <hwestman@arcticcircledc.com>
2019-01-25 08:50:02 +0100

    added keys

--- terraform/Mo/rancher-master/rancher-master.tf
+++ terraform/Mo/rancher-master/rancher-master.tf
@@ -1,7 +1,7 @@
 resource "openstack_compute_instance_v2" "rancherMaster" {
   name            = "rancherMaster"
   flavor_name     = "g1.large_lowmem"
-  key_pair        = "host-1"
+  key_pair        = "rancher-master"
   security_groups = ["acdc-secgroup-1",
     "${openstack_compute_secgroup_v2.rancherMaster_secgroup.name}",
     "${openstack_compute_secgroup_v2.rancher_cattle.name}"
@@ -23,7 +23,9 @@ resource "openstack_compute_instance_v2" "rancherMaster" {
     name = "${module.region.regional_public_network_name}"
   }
 
-  depends_on = ["openstack_blockstorage_volume_v1.rancherMaster_root_volume"]
+  depends_on = ["openstack_blockstorage_volume_v1.rancherMaster_root_volume"]  
+  depends_on = ["openstack_compute_keypair_v2.rancher_master_keypair"]
+ 
 }
 
 resource "openstack_blockstorage_volume_v1" "rancherMaster_root_volume" {
@@ -37,11 +39,36 @@ resource "openstack_blockstorage_volume_v1" "rancherMaster_root_volume" {
 resource "openstack_networking_floatingip_v2" "rancherMaster_floatip_1" {
   pool = "public_network"
 }
+resource "openstack_compute_keypair_v2" "rancher_node_keypair" {
+  name = "rancher-node"
+}
+
+resource "openstack_compute_keypair_v2" "rancher_master_keypair" {
+  name = "rancher-master"
+}
 
 resource "openstack_compute_floatingip_associate_v2" "rancherMaster_fip_attachment" {
   floating_ip = "${openstack_networking_floatingip_v2.rancherMaster_floatip_1.address}"
   instance_id = "${openstack_compute_instance_v2.rancherMaster.id}"
+
+  depends_on = ["openstack_compute_keypair_v2.rancher_node_keypair"]
+  provisioner "file" {
+    content     = "${openstack_compute_keypair_v2.rancher_node_keypair.private_key}"
+    destination = "~/rancher-node.pem"
+
+    connection {
+      type = "ssh"
+      host = "${openstack_networking_floatingip_v2.rancherMaster_floatip_1.address}"
+      user = "ubuntu"
+      private_key = "${openstack_compute_keypair_v2.rancher_master_keypair.private_key}"
+    }
+  }
 }
+
+output "Rancher-master private key" {
+  value = "${openstack_compute_keypair_v2.rancher_master_keypair.private_key}"
+}
+
 output "IP for Rancher" {
   value = "${openstack_networking_floatingip_v2.rancherMaster_floatip_1.address}"
 }
--- terraform/Mo/rancher-master/security_groups.tf
+++ terraform/Mo/rancher-master/security_groups.tf
@@ -15,6 +15,12 @@ resource "openstack_compute_secgroup_v2" "rancherMaster_secgroup" {
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
 }
 
 resource "openstack_compute_secgroup_v2" "rancher_cattle" {
