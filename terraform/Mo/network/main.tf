95e40baf42e52ed81f85d6c91dd873399e4914a4
Stian Brandt <sbra@arcticcircledc.com>
2019-09-24 18:29:46 +0200

    Run terraform 0.12upgrade on network

--- terraform/Mo/network/main.tf
+++ terraform/Mo/network/main.tf
@@ -4,13 +4,14 @@ module "global" {
 
 # Configure the OpenStack Provider
 provider "openstack" {
-  domain_name = "${module.global.domain}"
-  user_name   = "${module.global.username}"
-  tenant_name = "${module.global.domain}"
-  auth_url    = "${module.global.auth_url}"
+  domain_name = module.global.domain
+  user_name   = module.global.username
+  tenant_name = module.global.domain
+  auth_url    = module.global.auth_url
   region      = "RegionOne"
 }
 
 data "openstack_networking_network_v2" "public_network" {
   name = "public_network"
 }
+
--- terraform/Mo/network/networks.tf
+++ terraform/Mo/network/networks.tf
@@ -5,7 +5,7 @@ resource "openstack_networking_network_v2" "acdc_cloud_network" {
 
 resource "openstack_networking_subnet_v2" "acdc_subnet_1" {
   name       = "acdc-s-1"
-  network_id = "${openstack_networking_network_v2.acdc_cloud_network.id}"
+  network_id = openstack_networking_network_v2.acdc_cloud_network.id
   cidr       = "192.168.100.0/24"
   ip_version = 4
 
@@ -15,10 +15,11 @@ resource "openstack_networking_subnet_v2" "acdc_subnet_1" {
 resource "openstack_networking_router_v2" "acdc_router" {
   name                = "acdc-router"
   admin_state_up      = true
-  external_network_id = "${data.openstack_networking_network_v2.public_network.id}"
+  external_network_id = data.openstack_networking_network_v2.public_network.id
 }
 
 resource "openstack_networking_router_interface_v2" "acdc_router_interface_1" {
-  router_id = "${openstack_networking_router_v2.acdc_router.id}"
-  subnet_id = "${openstack_networking_subnet_v2.acdc_subnet_1.id}"
+  router_id = openstack_networking_router_v2.acdc_router.id
+  subnet_id = openstack_networking_subnet_v2.acdc_subnet_1.id
 }
+
