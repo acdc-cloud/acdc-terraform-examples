075d75ca1ba4b37ec58ef2c093aef39e613d9dfb
Stian Brandt <sbra@arcticcircledc.com>
2019-09-24 18:59:39 +0200

    Run terraform 0.12upgrade on rancher-master

--- terraform/Mo/rancher-master/security_groups.tf
+++ terraform/Mo/rancher-master/security_groups.tf
@@ -1,4 +1,4 @@
-resource "openstack_compute_secgroup_v2" "rancherMaster_secgroup" {
+resource "openstack_compute_secgroup_v2" "rancher_master_secgroup" {
   name        = "rancherMaster"
   description = "HTTP and HTTPS for rancher ingress"
 
@@ -15,7 +15,6 @@ resource "openstack_compute_secgroup_v2" "rancherMaster_secgroup" {
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
- 
 }
 
 resource "openstack_compute_secgroup_v2" "rancher_cattle" {
@@ -26,62 +25,63 @@ resource "openstack_compute_secgroup_v2" "rancher_cattle" {
     from_port   = 22
     to_port     = 22
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 443
     to_port     = 443
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 80
     to_port     = 80
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 6443
     to_port     = 6443
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 2376
     to_port     = 2376
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 2379
     to_port     = 2380
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 10250
     to_port     = 10256
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 30000
     to_port     = 32767
     ip_protocol = "tcp"
-    self = true
+    self        = true
   }
 
   rule {
     from_port   = 8472
     to_port     = 8472
     ip_protocol = "udp"
-    self = true
+    self        = true
   }
 }
+
