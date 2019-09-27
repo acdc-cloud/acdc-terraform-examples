16fc5b9a24875fdd1262c71e2ed89a50337a3e66
Stian Brandt <sbra@arcticcircledc.com>
2019-09-27 11:01:32 +0200

    Move dns example from global to a separate module in the arcticcloud folder

---
+++ terraform/arcticcloud/dns/main.tf
@@ -0,0 +1,12 @@
+module "global" {
+  source = "../global-vars"
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  domain_name = module.global.domain
+  user_name   = module.global.username
+  tenant_name = module.global.domain
+  auth_url    = module.global.auth_url
+  region      = "RegionOne"
+}
